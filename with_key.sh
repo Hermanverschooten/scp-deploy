7f2564f127fae1413af8e6c10378c25789ec9699
Herman verschooten <Herman@verschooten.net>
2021-05-06 21:21:30 +0200

    Re-add ssh-agent

--- with_key.sh
+++ with_key.sh
@@ -1,25 +1,32 @@
 #!/bin/sh -l
+set -x
 echo "🔑 Adding ssh key..." 
-echo -e "${INPUT_KEY}" >__TEMP_INPUT_KEY_FILE
+mkdir /root/.ssh
+echo -e "${INPUT_KEY}" >/root/.ssh/id_rsa
 
-chmod 600 __TEMP_INPUT_KEY_FILE
+chmod 600 /root/.ssh/id_rsa
+
+eval $(ssh-agent -s)
+ssh-add -k
+ssh-add -l
+echo "StrictHostKeyChecking no" > /root/.ssh/config
 echo "🔐 Added ssh key"
 
 PRE_UPLOAD=${INPUT_PRE_UPLOAD}
 if [ ! -z "$PRE_UPLOAD" ]; then
     echo "👌 Executing pre-upload script..." &&
-    ssh ${INPUT_SSH_OPTIONS} -p "${INPUT_PORT}" -i __TEMP_INPUT_KEY_FILE ${INPUT_USER}@${INPUT_HOST} "$INPUT_PRE_UPLOAD && exit" &&
+    ssh ${INPUT_SSH_OPTIONS} -p "${INPUT_PORT}" ${INPUT_USER}@${INPUT_HOST} "$INPUT_PRE_UPLOAD && exit" &&
     echo "✅ Executed pre-upload script";
 fi
 
 echo "🚚 Uploading via scp..." &&
-scp ${INPUT_SSH_OPTIONS} ${INPUT_SCP_OPTIONS} -i __TEMP_INPUT_KEY_FILE -P "${INPUT_PORT}" -r ${INPUT_LOCAL} ${INPUT_USER}@${INPUT_HOST}:"${INPUT_REMOTE}" &&
+scp ${INPUT_SSH_OPTIONS} ${INPUT_SCP_OPTIONS} -P "${INPUT_PORT}" -r ${INPUT_LOCAL} ${INPUT_USER}@${INPUT_HOST}:"${INPUT_REMOTE}" &&
 echo "🙌 Uploaded via scp";
 
 POST_UPLOAD=${INPUT_POST_UPLOAD}
 if [ ! -z "$POST_UPLOAD" ]; then
     echo "👌 Executing post-upload script..." &&
-    ssh ${INPUT_SSH_OPTIONS} -p "${INPUT_PORT}" -i __TEMP_INPUT_KEY_FILE ${INPUT_USER}@${INPUT_HOST} "$POST_UPLOAD && exit" &&
+    ssh ${INPUT_SSH_OPTIONS} -p "${INPUT_PORT}" ${INPUT_USER}@${INPUT_HOST} "$POST_UPLOAD && exit" &&
     echo "✅ Executed post-upload script";
 fi
 
